2041b2bf4ad45c8da3cd2040e06e3e6ca328dbc8
Tomer <tomer.zilberman.44@gmail.com>
2025-04-06 16:41:25 +0300

    fix 3

--- drop.sql
+++ drop.sql
@@ -1,5 +1,6 @@
-drop table if exists Country cascade;
-drop table if exists Year cascade;
-drop table if exists Region cascade;
-drop table if exists IncomeGroup cascade;
+drop table if exists GotIn cascade;
 drop table if exists University cascade;
+drop table if exists Country cascade;
+drop table if exists IncomeGroup cascade;
+drop table if exists Region cascade;
+drop table if exists Year cascade;
